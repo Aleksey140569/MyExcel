84532d796f8a66ab4db60261112e7a184086940b
Aleksey140569 <stranger140569@yandex.ru>
2024-03-19 23:58:34 +0300

    upadate
    upadate code

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,39 @@
 import {createTable} from '@/components/table/table.template';
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: [
+        'mousedown',
+      ]
+    })
+  }
+
   toHTML() {
     return createTable(45);
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target);
+      const $parent = $resizer.closest('[data-type="resizeable"]');
+      const coords = $parent.getCoords();
+
+      document.onmousemove = (e) => {
+        const delta = Math.floor(e.pageX - coords.right);
+        const value = coords.width + delta;
+        $parent.$el.style.width = value + 'px';
+        document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
+            .forEach((el) => el.style.width = value + 'px');
+      }
+
+      document.onmouseup = (e) => {
+        document.onmousemove = null;
+      }
+    }
   }
 }
