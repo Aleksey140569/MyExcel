8146eda1f1c348607b6b72506561368458f7b026
Aleksey140569 <stranger140569@yandex.ru>
2024-04-16 19:07:11 +0300

    Finish base table logic
    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,11 +4,24 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribes = [];
+    this.prepare();
   }
 
-  // Возвращает шаблон компонета.
+  prepare() {}
+
   toHtml() {
     return '';
+  }
+
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribes.push(unsub);
   }
 
   init() {
@@ -17,5 +30,6 @@ export class ExcelComponent extends DomListener {
 
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribes.forEach((unsub) => unsub());
   }
 }
